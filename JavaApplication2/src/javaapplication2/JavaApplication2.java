f754ffb8d4024e92913b33200750bb7284b281d3
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-01-25 18:55:36 +0600

    add
    java grammar

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,54 @@
+
+package javaapplication2;
+
+import java.util.Scanner;
+
+
+public class JavaApplication2 {
+
+   
+    public static void main(String[] args) {
+//        
+//  Scanner input = new Scanner(System.in);
+//  
+//        System.out.println("Enter Second ");
+//     
+//        int seconds=input.nextInt();
+//        
+//        int minutes = seconds /60;
+//        int second = seconds % 60;
+//        
+//        System.out.println(minutes + " Minute " + second + " Second ");
+
+//Scanner time = new Scanner(System.in);
+//        System.out.println("Enter Second");
+//        int second = time.nextInt();
+//        
+//        int minute = second/60;
+//        int secounds = second%60;
+//        
+//        System.out.println(minute + " Minute " + secounds +" Second ");
+        
+        
+        
+        Scanner time = new Scanner(System.in);
+        System.out.println("Enter Minute");
+        int minite = time.nextInt();
+        
+        int seconds = minite*60;
+        
+        
+        System.out.println(  seconds +" Second ");
+           
+
+
+
+
+
+
+
+        
+        
+    }
+    
+}
