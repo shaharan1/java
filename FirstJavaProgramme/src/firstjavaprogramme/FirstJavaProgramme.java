20d4cb1410107b5aaa694d26c6e0f7bc158a40ba
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-01-25 16:52:48 +0600

    add
    zszz

---
+++ FirstJavaProgramme/src/firstjavaprogramme/FirstJavaProgramme.java
@@ -0,0 +1,57 @@
+
+package firstjavaprogramme;
+
+import java.util.Scanner;
+
+
+public class FirstJavaProgramme {
+
+   
+    public static void main(String[] args) {
+        
+     Scanner input = new Scanner(System.in);
+        System.out.println("What is Your Name? ");
+     String name = input.nextLine();
+     
+//     String output= name.concat(name) ;
+        System.out.println(" My Name is " + name );
+        
+        
+             
+//        Scanner input = new Scanner(System.in);
+//        
+//        Scanner abul = new Scanner(System.in);
+//        
+//        Scanner masud = new Scanner(System.in);
+//        
+//        Scanner tanvir = new Scanner(System.in);
+//        
+//        Scanner pk = new Scanner(System.in);
+//        
+//           double area;
+//        System.out.println("Pls Enter Radius:-");
+//        float radius = input.nextFloat();
+//        area= 3.1416 * radius * radius;
+//         System.out.println("Area of a circle is " + area);
+//        
+////     float radius;
+//     double area;
+//     radius = 5;
+//     
+//     
+//area= 3.1416 * radius * radius;
+//        System.out.println("Area of a circle is" + area);
+
+
+
+//float radius;
+//     float area;
+//     radius = 5;
+//     
+//     
+//area= (float)3.1416 * radius * radius;
+//        System.out.println("Area of a circle is" + area);
+//     
+    }
+    
+}
