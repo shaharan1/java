791b60583771bb95aeac41f1cdbd0096359a29db
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-01-26 18:53:20 +0600

    add
    3rd Class java

---
+++ ThirdDayJavaPractice/src/thirddayjavapractice/ThirdDayJavaPractice.java
@@ -0,0 +1,52 @@
+package thirddayjavapractice;
+
+import java.util.Scanner;
+
+public class ThirdDayJavaPractice {
+
+    public static void main(String[] args) {
+//    int a = 5;
+//        a += a;
+//        System.out.println(a);
+//        
+//        a -= 3;
+//        System.out.println(a);
+//        
+//        a *= 5;
+//        System.out.println(a);
+//        
+//        a /= 2;
+//        System.out.println(a);
+//
+//        a %= 2;
+//        System.out.println(a);
+//        
+//        int i =2;
+//        System.out.println("a++ = " + a++);
+//        System.out.println("++a = " + ++a);
+//        System.out.println("a-- = " + a--);
+//        System.out.println("--a = " + --a);      
+
+//        Scanner input = new Scanner(System.in);
+//        
+//        System.out.println("Enter First Input");
+//        int first = input.nextInt();
+//        
+//        System.out.println("Enter Second Input");
+//        int second = input.nextInt();
+//        
+//       
+//        System.out.println("The Result = " + (first + second));
+       
+       
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String name = input.nextLine();
+        System.out.println("Welcome to Java Developer " + name);
+
+        
+        
+        
+    }
+
+}
