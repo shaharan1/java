4f5097130235293e925d765895157d53bf4f8a51
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-02-07 22:52:26 +0600

    add

--- RoomPracticeArray/src/roompracticearray/RoomPracticeArray.java
+++ RoomPracticeArray/src/roompracticearray/RoomPracticeArray.java
@@ -1,20 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package roompracticearray;
 
-/**
- *
- * @author MSI
- */
+import java.util.Scanner;
+
 public class RoomPracticeArray {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        int numbers[][] = new int[3][4];
+//        numbers[0][0] = 1;
+//        numbers[0][1] = 2;
+//        numbers[0][2] = 3;
+//        numbers[0][3] = 4;
+//
+//        numbers[1][0] = 5;
+//        numbers[1][1] = 6;
+//        numbers[1][2] = 7;
+//        numbers[1][3] = 8;
+//
+//        numbers[2][0] = 9;
+//        numbers[2][1] = 10;
+//        numbers[2][2] = 11;
+//        numbers[2][3] = 12;
+//
+//        for (int row = 0; row < numbers.length; row++) {
+//            for (int col = 0; col < numbers[row].length; col++) {
+//                System.out.print(numbers[row][col] + "\t");
+//            }
+//            System.out.println("");
+//        }
+
+        Scanner s=new Scanner(System.in);
+        System.out.println("");
     }
-    
+
 }
