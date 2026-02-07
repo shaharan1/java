025fbb2656518b07e2f1bf70ad3c1897e9616832
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-02-07 18:58:00 +0600

    add

---
+++ LearnObjectClass/src/learnobjectclass/geometry/RectAngle.java
@@ -0,0 +1,25 @@
+
+package learnobjectclass.geometry;
+
+
+public class RectAngle {
+    
+  public double hight;
+    public double width;
+    
+    public double getArea(){    
+            
+        return hight*width;
+        
+    }
+    
+    public double getPerimeter(){
+       
+    return 2*(hight+width);
+    
+    }
+
+
+    
+    
+}
