2017dc2e999d9c0ecaff0167d17fea6a9ced7b24
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-02-19 01:25:40 +0600

    add

---
+++ ExamPractice/src/exampractice/ExamPractice.java
@@ -0,0 +1,23 @@
+
+package exampractice;
+
+import java.util.Scanner;
+
+
+public class ExamPractice {
+
+   
+    public static void main(String[] args) {
+//        Scanner s=new Scanner(System.in);
+//         System.out.println("Enter Your Number");
+//        int input=s.nextInt();
+    
+       int factorial=1;
+       for(int i=1;i<=5;i++){
+       factorial*=i;
+       }
+        System.out.println("Factorial of "+ 5 +" is "+factorial);
+        
+    }
+    
+}
