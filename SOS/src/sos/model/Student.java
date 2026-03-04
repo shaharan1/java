ca9cebe7b30bfd9c24227b3d14a9d13b7cd5d6d7
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-03-04 16:30:12 +0600

    add

--- SOS/src/sos/model/Student.java
+++ SOS/src/sos/model/Student.java
@@ -1,7 +1,26 @@
-
 package sos.model;
 
-
 public class Student {
-    
+
+    int stuId;
+    String stuName;
+    String stuGmail;
+    String stuCell;
+
+    public Student() {
+    }
+
+    public Student(int stuId, String stuName, String stuGmail, String stuCell) {
+        this.stuId = stuId;
+        this.stuName = stuName;
+        this.stuGmail = stuGmail;
+        this.stuCell = stuCell;
+    }
+
+    public Student(String stuName, String stuGmail, String stuCell) {
+        this.stuName = stuName;
+        this.stuGmail = stuGmail;
+        this.stuCell = stuCell;
+    }
+
 }
